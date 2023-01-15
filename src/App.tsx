74a6a5d6d55e94723f635a4c1012787d1e17fd3d
Tea <controlalttea@gmail.com>
2023-01-15 10:08:05 -0800

    adding props to the Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import NavBar from "@/scenes/navbar"
 
 function App() {
-
+const {selectedPage, setSelectedPage} = useState()
   return (
     <div className="app bg-gray-20">
-      <NavBar />
+      <NavBar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link";
 
-type Props = {};
 
-const NavBar = (props: Props) => {
+type Props = {
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+};
+
+const NavBar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between"
 
     return (
@@ -16,10 +21,26 @@ const NavBar = (props: Props) => {
                         {/* RIGHT SIDE */}
                         <div className={`${flexBetween} w-full`}>
                             <div className={`${flexBetween} gap-8 text-sm`}>
-                                <p>Home</p>
-                                <p>Benefits</p>
-                                <p>Our Classes</p>
-                                <p>Contact Us</p>
+                                <Link
+                                    page="Home"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Benefits"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Our Classes"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Contact Us"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
                             </div>
                             <div className={`${flexBetween} gap-8`}>
                                 <p>Sign In</p>
