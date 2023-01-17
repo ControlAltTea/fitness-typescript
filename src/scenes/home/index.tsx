7c8fbacb264d5e3572c9d239927d851eb9c458ed
Tea <controlalttea@gmail.com>
2023-01-16 16:35:40 -0800

    rearranging divs to adjust flex

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -2,7 +2,7 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
 import HomePageText from "@/assets/HomePageText.png";
-import HomeGraphic from "@/assets/HomeGraphic.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBul from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
@@ -12,23 +12,23 @@ type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Home = ({setSelectedPage}: Props) => {
+const Home = ({ setSelectedPage }: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-   
+
     return (
-        <section
-            id="home"
-            className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
-        >
-        {/* IMAGE AND MAIN HEADER */}
-            <div>
+        <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+            {/* IMAGE AND MAIN HEADER */}
+            <div className="mx-auto w-5/6 items-center justify-center md:h-5/6">
                 {/* MAIN HEADER */}
-                <div>
-                    {/* HEADINGS */}     
-                    <div>
+                <div className="z-10 mt-32 md:basis-3/5">
+                    {/* HEADINGS */}
+                    <div className="md:-mt-20">
                         <div>
-                            <img src={HomePageText} alt="home-page-text" />
+                            <div>
+                                <img src={HomePageText} alt="home-page-text" />
+                            </div>
                         </div>
+                        
                         <p>
                             Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
                             Studios to get the Body Shapes That you Dream of.. Get Your Dream
@@ -38,24 +38,34 @@ const Home = ({setSelectedPage}: Props) => {
                     {/* ACTIONS */}
                     <div>
                         <ActionButton
-                        setSelectedPage={setSelectedPage}>
+                            setSelectedPage={setSelectedPage}>
                             Join Now
                         </ActionButton>
                         <AnchorLink
                             className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
                             onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-                        href={`#${SelectedPage.ContactUs}`}>
+                            href={`#${SelectedPage.ContactUs}`}>
                             <p>Learn More</p>
                         </AnchorLink>
                     </div>
                 </div>
                 {/* IMAGE */}
                 <div>
-                    <img src={HomeGraphic} alt="home-pageGraphic" />
+                    <img src={HomePageGraphic} alt="home-pageGraphic" />
                 </div>
             </div>
             {/* SPONSORS */}
-            
+            {isAboveMediumScreens && (
+                <div>
+                    <div>
+                        <div>
+                            <img src={SponsorRedBul} alt="redbull-sponsor" />
+                            <img src={SponsorForbes} alt="forbes-sponsor" />
+                            <img src={SponsorFortune} alt="fortune-sponsor" />
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
     )
 }
