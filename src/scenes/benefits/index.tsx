74b9f17a5523731f5ef3bcea68c3c7e6b1e0b918
Tea <controlalttea@gmail.com>
2023-01-18 11:16:30 -0800

    moving onto the benefits section with the 3 cards

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -21,12 +21,15 @@ const Benefits = ({setSelectedPage}: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div className="md:my-5 md:w-3/5">
+        <div className="md:my-5 md:w-3/">
           <HText>MORE THAN JUST A GYM</HText>
           <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
-            care into each and every member.</p>
+            care into each and every member.
+          </p>
         </div>
+        {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5"></div>
       </motion.div>      
     </section>
   )
