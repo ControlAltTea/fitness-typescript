69aeadba3143c9ed8772d8b2b76457408f648b38
Tea <controlalttea@gmail.com>
2023-01-19 12:26:53 -0800

    animations added to benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -95,23 +95,41 @@ const Benefits = ({setSelectedPage}: Props) => {
             {/* TITLE */}
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
-                <div>
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: {opacity: 1, x: 0}
+                  }}
+                >
                   <HText>MILLIONS OF HAPPY MEMBERS GETTING {" "}
                   <span className="text-primary-500">FIT</span>
                   </HText>
-                </div>
+                </motion.div>
               </div>
             </div>
 
             {/* DESCRIPTION */}
-            <div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: {opacity: 1, x: 0}
+              }}
+            >
               <p className="my-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque culpa suscipit facilis autem assumenda quis nisi totam natus praesentium est tempore eum commodi maiores, recusandae mollitia ratione delectus distinctio rem?
               </p>
               <p className="mb-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti commodi, enim distinctio sunt, quae asperiores voluptatibus officiis rerum, veritatis molestias libero quaerat nam assumenda sint expedita debitis voluptatum beatae dolores!
               </p>
-            </div>
+            </motion.div>
 
             {/* BUTTON */}
             <div className="relative mt-16">
