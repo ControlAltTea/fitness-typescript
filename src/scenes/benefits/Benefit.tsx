b9fac7b2f3f9dd9b12c83ce26f0c78c066721f48
Tea <controlalttea@gmail.com>
2023-01-19 11:24:33 -0800

    added motion div to the container around the benefits section

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -25,16 +25,16 @@ const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
     <motion.div
       variants={childVariant}
       className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16'>
-          <div className="mb-4 flex justify-center">
-              <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
-                {icon}
-              </div>
-          </div>
-          <h4 className="font-bold">{title}</h4>
-          <p className="my-3">{description}</p>
-          <AnchorLink>
-               
-          </AnchorLink>
+        <div className="mb-4 flex justify-center">
+            <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
+              {icon}
+            </div>
+        </div>
+        <h4 className="font-bold">{title}</h4>
+        <p className="my-3">{description}</p>
+        <AnchorLink>
+              
+        </AnchorLink>
     </motion.div>
   )
 }
