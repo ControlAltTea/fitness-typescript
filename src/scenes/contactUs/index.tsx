308bc1b1226603f726b5369d85fd3f4703f6f2d6
Tea <controlalttea@gmail.com>
2023-01-20 17:27:55 -0800

    form nearly done

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,6 +9,9 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+    const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+  px-5 py-3 placeholder-white`;
+
     const {
         register,
         trigger,
@@ -58,13 +61,62 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                           visible: {opacity: 1, y: 0},
                       }}
                   >
-                      <form
+                     <form
                           target="_blank"
                           onSubmit={onSubmit}
                           method="POST"
-                          action={`{https://formsubmit.com/${your@email.com}`}
+                          action={`{https://formsubmit.com/your@email.com`}
                       >
-                          
+                          <input className={`${inputStyles}`}
+                              type="text"
+                              placeholder="NAME"
+                              {...register("name", {
+                                  required: true,
+                                  maxLength: 100,
+                              })}
+                          >
+                              {errors.name && (
+                                  <p className="mt-1 text-primary-500">
+                                      {errors.name.type === "required" && "This field is required."}
+                                        {errors.name.type === "maxLength" && "Max length is 100 characters."}
+                                  </p>
+                            )}  
+                          </input>
+                           <input className={`${inputStyles}`}
+                              type="text"
+                              placeholder="EMAIL"
+                              {...register("email", {
+                                  required: true,
+                                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                              })}
+                          >
+                              {errors.email && (
+                                  <p className="mt-1 text-primary-500">
+                                      {errors.email.type === "required" && "This field is required."}
+                                        {errors.email.type === "pattern" && "Invalid email address."}
+                                  </p>
+                            )}  
+                        </input>
+                        <input className={inputStyles}
+                            type="text"
+                            placeholder="MESSAGE"
+                            {...register("message", {
+                                required: true,
+                                maxLength: 2000,
+                            })}
+                        >
+                        {errors.message && (
+                            <p className="mt-1 text-primary-500">
+                                {errors.message.type === "required" && "This field is required."}
+                                {errors.message.type === "maxLength" && "Max length is 2000 characters."}
+                            </p>
+                        )}  
+                          </input>
+                          <button type="submit"
+                            className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                          >
+                              
+                          </button>
                       </form>
                   </motion.div>
               </div>
