a0e64130eb3bb38d8299f100922c108dcfe74d65
Tea <controlalttea@gmail.com>
2023-01-22 12:29:50 -0800

    contact info added

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -9,11 +9,10 @@ const Footer = () => {
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores libero natus, accusantium suscipit quas quaerat repellat. Dignissimos consectetur atque facilis aperiam assumenda. Illum ex, possimus eveniet voluptatibus nostrum reprehenderit ad.</p>
                   <p>Ⓒ Evogym All Rights Reserved.</p>
             </div>
-              <div>
-                  <h4 className="font-bold">Links</h4>
-                  <p className="my-5">Massa orci senectus</p>
-                  <p className="my-5">Et gravida id et etiam</p>
-                  <p></p>
+              <div className="mt-16 basis-1/4 md:mt-0">
+                  <h4 className="font-bold">Contact Us</h4>
+                  <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+                  <p>(333) 425-6825</p>
               </div>
             <div></div>
           </div> 
